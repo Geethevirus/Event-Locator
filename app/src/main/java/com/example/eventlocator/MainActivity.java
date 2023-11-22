6350ef1f4997e4cab83d58d3d8f06f7931a74859
Gontse.seaga <124888584+Geethevirus@users.noreply.github.com>
2023-11-22 03:50:49 +0200

    update
    Connecting the views to the dialog class

--- app/src/main/java/com/example/eventlocator/MainActivity.java
+++ app/src/main/java/com/example/eventlocator/MainActivity.java
@@ -7,7 +7,16 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.eventlocator.databinding.ActivityMainBinding;
 
@@ -42,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
                     replaceFragment(new SearchFragment());
                     break;
                 case R.id.add_posts:
+                    showBottomDialog();
                     break;
                 case R.id.exploreicon:
                     replaceFragment(new ExploreFragment());
@@ -65,6 +75,36 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.commit();
 
     //Siyabonga Radebe
+
+    }
+    private void showBottomDialog()
+    {
+        final Dialog dialog = new Dialog(this);
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottom_sheet);
+
+
+        LinearLayout uploadPic = dialog.findViewById(R.id.upload_pictLay);
+
+        uploadPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Toast.makeText(getApplicationContext(),"Function Not available",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
+
+
+
 
     }
 }
